b2fb83d1332677302795e28c445fe6bb0a2d5b0d
James Dewes <james@jamesdewes.com>
2016-04-27 00:55:53 +0100

    added fan object

---
+++ fan/__init__.py
@@ -0,0 +1,32 @@
+__author__ = 'James'
+import RPi.GPIO as gpio
+def setup():
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        #21 is not used by the PyGlow on the Pi  B
+        GPIO.setup(21, GPIO.OUT)
+        return True
+    except Exception:
+        raise Exception("unable to set up")
+        return False
+
+
+def start():
+    #start the fan
+    try:
+        GPIO.output(port_or_pin, 1)
+        return True
+    except Exception:
+        raise Exception("unable to start")
+        return False
+
+def stop():
+    #start the fan
+    try:
+        GPIO.output(port_or_pin, 0)
+        return True
+    except Exception:
+        raise Exception("unable to stop")
+        return False
+
