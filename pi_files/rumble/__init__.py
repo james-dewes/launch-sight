93668266d01f46231a4cdafeb5208a273aa57bc1
SUMMITMEDIA\james.dewes <james.dewes@summitmedia.com>
2016-11-01 17:48:37 +0000

    rewrote this as a self contained class

--- pi_files/rumble/__init__.py
+++ pi_files/rumble/__init__.py
@@ -1,39 +1,42 @@
-__author__ = 'James'
-import bluetooth
-rumble_pack_address = None
-server_socket = None
-def setup():
-    try:
-        global rumble_pack_address
-        rumble_pack_name = "HC-05"
-        for retry in range(0,5):
-            nearby_devices = bluetooth.discover_devices()
-            for bdaddr in nearby_devices:
-                if rumble_pack_name == bluetooth.lookup_name(bdaddr):
-                    rumble_pack_address = bdaddr
-                    print("found rumble_pack")
-                    break
-                
-            
-            if rumble_pack_address == None:
-                print("rumble_pack not found, retrying...")
-            else:
-                break          
-
-           
-    except Exception as err:
-         print(err)
+__author__ = 'James Dewes'
 
 
-def start():
-    port = 1
-    global server_socket
-    server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    server_socket.connect((rumble_pack_address,port))
-    server_socket.send("1")
+class rumble:
+    def __init__(self):
+        try:
+            import bluetooth
+        except RuntimeError:
+            print("Unable to import blutooth. bluetooth module not installed?")
+            raise Exception("Unable to import blutooth. bluetooth module not installed?")
 
-def stop():
-    try:
-	server_socket.close()
-    except Exception:
-	raise Exception("unable to stop")
+        self.rumble_pack_address = None
+        self.server_socket = None
+        self.rumble_pack_name = "HC-05"
+        try:
+
+            for retry in range(0, 5):
+                self.nearby_devices = bluetooth.discover_devices()
+                for bdaddr in nearby_devices:
+                    if rumble_pack_name == bluetooth.lookup_name(bdaddr):
+                        self.rumble_pack_address = bdaddr
+                        print("found rumble_pack")
+                        break
+                    if self.rumble_pack_address == None:
+                        print("rumble_pack not found, retrying...")
+                    else:
+                        break
+
+        except Exception as err:
+            print(err)
+
+    def start(self):
+        port = 1
+        self.server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.server_socket.connect((self.rumble_pack_address,port))
+        self.server_socket.send("1")
+
+    def stop(self):
+        try:
+            self.server_socket.close()
+        except Exception:
+            raise Exception("unable to stop")
