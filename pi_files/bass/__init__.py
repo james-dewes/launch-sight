c143173ad82addb1f5de62caf4a52eafbba90b56
SUMMITMEDIA\james.dewes <james.dewes@summitmedia.com>
2016-11-01 17:47:11 +0000

    repackeged this as a class

--- pi_files/bass/__init__.py
+++ pi_files/bass/__init__.py
@@ -1,23 +1,23 @@
-__author__ = 'James'
-import pygame
-from pygame.mixer import Sound
-soundfile = "launch_bass_boost_long.wav"
-bass = None
-def setup():
-    try:
-        global bass
-        pygame.mixer.init()
-        bass = Sound(soundfile)
-    except Exception:
-        raise Exception("unable to set up")
+class bass:
+    __author__ = 'James Dewes'
+    def __init__(self):
+        try:
+            from pygame.mixer import Sound
+        except RuntimeError:
+            print("Unable to import pygame mixer sound")
+            raise Exception("Unable to import pygame mixer sound")
 
-def start():
-    state = bass.play()
-    while state.get_busy() == True:
-        continue    
+        self.soundfile = "launch_bass_boost_long.wav"
+        self.mixer = pygame.mixer.init() #instance of pygame
+        self.bass = Sound(soundfile)
 
-def stop():
-    try:
-	None
-    except Exception:
-	raise Exception("unable to stop")
+    def start(self):
+        state = bass.play()
+        while state.get_busy() == True:
+            continue
+
+    def stop(self):
+        try:
+            Pass
+        except Exception:
+            raise Exception("unable to stop")
