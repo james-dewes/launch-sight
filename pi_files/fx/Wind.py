027e8199cdd20f48f9a027cbb4395c58483d95b9
James Dewes <james@jamesdewes.com>
2016-11-05 22:39:09 +0000

    Updated wind to run a stepper motor

--- pi_files/fx/Wind.py
+++ pi_files/fx/Wind.py
@@ -1,35 +1,79 @@
+#!/usr/bin/python
+#--------------------------------------
+#    ___  ___  _ ____
+#   / _ \/ _ \(_) __/__  __ __
+#  / , _/ ___/ /\ \/ _ \/ // /
+# /_/|_/_/  /_/___/ .__/\_, /
+#                /_/   /___/
+#
+#    Stepper Motor Test
+#
+# A simple script to control
+# a stepper motor.
+#
+# Author : Matt Hawkins
+# Date   : 28/09/2015
+# Modified: James Dewes
+# Date   : 05/11/2016
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+#--------------------------------------
 class Wind:
-    __author__ = 'James Dewes'
-    def __int__(self, pin=21):
-        self.pin = pin
-        try:
-            import RPi.GPIO as GPIO
-        except RuntimeError:
-            print("Unable to import GPIO")
-            raise Exception("Unable to import GPIO")
+    def ___init__(self):
+        # Import required libraries
+        import sys
+        import time
+        import RPi.GPIO as GPIO
+        self.GPIO = GPIO
+        self.GPIO.setmode(GPIO.BCM)
 
-        try:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setwarnings(False)
-            #21 is not used by the PyGlow on the Pi  B
-            GPIO.setup(self.pin, GPIO.OUT)
-        except Exception as error:
-            print(error)
-            raise Exception("unable to set up" + repr(error))
+        # GPIO17,GPIO22,GPIO23,GPIO24
+        StepPins = [17,22,23,24]
 
-    def start(self):
-        #start the fan
-        try:
-            GPIO.output(self.pin, 1)
-        except:
-            raise Exception("unable to start")
+        # Set all pins as output
+        for pin in StepPins:
+          self.GPIO.setup(pin,self.GPIO.OUT)
+          self.GPIO.output(pin, False)
+
+        # Define advanced sequence
+        # as shown in manufacturers datasheet
+        self.Seq = [[1,0,0,1],
+               [1,0,0,0],
+               [1,1,0,0],
+               [0,1,0,0],
+               [0,1,1,0],
+               [0,0,1,0],
+               [0,0,1,1],
+               [0,0,0,1]]
+
+        self.StepCount = len(Seq)
 
 
-    def stop(self):
-        #stop the fan
-        try:
-            GPIO.output(self.pin, 0)
-            GPIO.cleanup()
-        except:
-            raise Exception("unable to stop")
+     = 1 # Set to 1 or 2 for clockwise
+                # Set to -1 or -2 for anti-clockwise
 
+     WaitTime = 10/float(1000)
+
+    # Initialise variables
+    StepCounter = 0
+
+    def start(self, StepDir 1):
+        # Start main loop
+        while True:
+            for pin in range(0, 4):
+                xpin = selfStepPins[pin]
+                if Seq[StepCounter][pin]!=0:
+                    self.GPIO.output(xpin, True)
+                else:
+                    self.GPIO.output(xpin, False)
+                    StepCounter += StepDir
+
+            # If we reach the end of the sequence
+            # start again
+            if (StepCounter>=StepCount):
+                break;
+            if (StepCounter<0):
+                StepCounter = StepCount+StepDir
+
+            time.sleep(WaitTime)
